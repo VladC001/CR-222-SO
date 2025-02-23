fc70c59bbc48472567fb0fcdcd10cd0e58f44038
Numele Tău <gogu.elena@ishunea.io>
2025-02-23 22:14:11 +0200

    Timer1 update

--- SO_Lab1/src/MultiTimer/Timer1.java
+++ SO_Lab1/src/MultiTimer/Timer1.java
@@ -1,14 +1,23 @@
 package MultiTimer;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Timer1 {
     private Timer timer;
     private Main app;
+    private JButton startButton;
+    private JButton stopButton;
 
-    public Timer1(Main app) {
+    private Color[] colors = {Color.RED, Color.GREEN, Color.YELLOW, Color.CYAN, Color.MAGENTA};
+    private int colorIndex = 0;
+
+    public Timer1(Main app, JButton startButton, JButton stopButton) {
         this.app = app;
+        this.startButton = startButton;
+        this.stopButton = stopButton;
     }
 
     public void start() {
@@ -16,15 +25,17 @@ public class Timer1 {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                app.logMessage("Timer 1: Se execută la fiecare 2 secunde");
+                app.changeButtonColor(startButton, colors[colorIndex]);
+                app.changeButtonColor(stopButton, colors[colorIndex]);
+
+                colorIndex = (colorIndex + 1) % colors.length;
             }
-        }, 0, 2000);
+        }, 0, 1000);
     }
 
     public void stop() {
         if (timer != null) {
             timer.cancel();
-            app.logMessage("Timer 1 oprit");
         }
     }
 }
