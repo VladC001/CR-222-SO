9f3224e5a0111cc9bebbbc9e55ad8eaa3d326bd3
VanilkaWvv <valentinprepelita3@gmail.com>
2025-03-17 01:33:28 +0200

    Version 0.0.1 (Comment added)

--- Lab2/src/main/java/org/lab/Main.java
+++ Lab2/src/main/java/org/lab/Main.java
@@ -9,14 +9,17 @@ public class Main {
         x = 25;
         y = 34;
         z = 20;
+        //Frame
         JFrame frame = new JFrame("Readers and Writers");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //Text area for view processes
         JTextArea textArea = new JTextArea();
         textArea.setText("Start\n");
         textArea.setFont(new Font("Arial", Font.PLAIN, 12));
         textArea.setEditable(false);
 
+        //scroll pane for view all history
         JScrollPane scrollPane = new JScrollPane(textArea);
 
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -25,8 +28,10 @@ public class Main {
         frame.setVisible(true);
         frame.setSize(500, 500);
 
+        //none functional
         Bibliotecary bibliotecary = new Bibliotecary();
 
+        //initialize y readers
         for (int i = 0; i < y; i++) {
             new Readers(textArea, bibliotecary, z).start();
         }
--- Lab2/src/main/java/org/lab/Readers.java
+++ Lab2/src/main/java/org/lab/Readers.java
@@ -18,25 +18,32 @@ public class Readers extends Thread {
     public void run() {
         Random rand = new Random();
         synchronized (this) {
+            //Unique id for readers
             id = count++;
             textArea.setEditable(true);
+            //Notify to start session
             textArea.append("Reader(" + id + ") start session\n");
             textArea.setEditable(false);
         }
         try {
+            //wait before start reading
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        //initialize contour
         int n = 0;
         while (n < z){
             try {
+                //display current book to read
                 textArea.setEditable(true);
                 textArea.append("Reader(" + id + ") start reading " + n + " book\n");
                 textArea.setEditable(false);
 
+                //wait random times between 0.5 and 3 seconds
                 Thread.sleep(500 + rand.nextInt(3000 - 500 + 1));
 
+                //display end of reading book
                 textArea.setEditable(true);
                 textArea.append("Reader(" + id + ") end reading " + n + " book\n");
                 textArea.setEditable(false);
@@ -45,6 +52,7 @@ public class Readers extends Thread {
             }
             n++;
         }
+        //display end of session
         textArea.setEditable(true);
         textArea.append("Reader("+id+") end session\n");
         textArea.setEditable(false);
