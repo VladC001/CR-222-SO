a327caf8908975f862e0cfe9e31fd6fb1af85299
KiryuZero0 <125077343+KiryuZero0@users.noreply.github.com>
2025-03-17 12:05:28 +0200

    ver.0.0.5

--- Lab2/src/main/java/org/lab/Bibliotecary.java
+++ Lab2/src/main/java/org/lab/Bibliotecary.java
@@ -1,15 +1,31 @@
 package org.lab;
 
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Bibliotecary {
+    private final ReentrantLock libraryLock = new ReentrantLock();
+
+
+    public void enterLibrary() throws InterruptedException {
+        if (!libraryLock.tryLock()) {
+            Thread.currentThread().interrupt();
+            throw new InterruptedException("Biblioteca este ocupată. Thread-ul curent a fost întrerupt.");
+        }
+    }
+
+    public void exitLibrary() {
+        if (libraryLock.isHeldByCurrentThread()) {
+            libraryLock.unlock();
+        }
+    }
+
     public void readBook() throws InterruptedException {
         Random rand = new Random();
         Thread.sleep(500 + rand.nextInt(3000 - 500 + 1));
     }
 
-
-    public synchronized void writeBook() throws InterruptedException {
+    public void writeBook() throws InterruptedException {
         Random rand = new Random();
         Thread.sleep(2000 + rand.nextInt(3000));
     }
--- Lab2/src/main/java/org/lab/Writers.java
+++ Lab2/src/main/java/org/lab/Writers.java
@@ -12,11 +12,9 @@ public class Writers extends Thread {
     private StyledDocument doc;
     private Bibliotecary bibliotecary;
 
-
     private static int totalBooksWritten = 0;
     private static int maxBooks = 0;
     private static boolean finishMessagePrinted = false;
-
 
     public Writers(StyledDocument doc, Bibliotecary bibliotecary, int maxBooks) {
         this.doc = doc;
@@ -32,67 +30,75 @@ public class Writers extends Thread {
 
     @Override
     public void run() {
-
+        boolean acquiredLock = false;
         Style styleGreen = doc.addStyle("GreenStyle", null);
         StyleConstants.setForeground(styleGreen, Color.GREEN);
         StyleConstants.setBold(styleGreen, true);
 
         try {
+            // Încercăm să intrăm în bibliotecă folosind metoda care utilizează ReentrantLock.
+            bibliotecary.enterLibrary();
+            acquiredLock = true;
             doc.insertString(doc.getLength(), "Writer(" + id + ") a intrat în bibliotecă.\n", null);
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
 
-        while (true) {
-            int currentBook;
+            while (true) {
+                int currentBook;
 
-            synchronized (Writers.class) {
-                if (totalBooksWritten >= maxBooks) {
-                    break; // Nu mai sunt cărți de scris
+                synchronized (Writers.class) {
+                    if (totalBooksWritten >= maxBooks) {
+                        break; // Nu mai sunt cărți de scris
+                    }
+                    totalBooksWritten++;
+                    currentBook = totalBooksWritten;
                 }
-                totalBooksWritten++;
-                currentBook = totalBooksWritten;
-            }
 
-
-            try {
-                doc.insertString(doc.getLength(), "Writer(" + id + ") a început să scrie cartea " + currentBook + ".\n", styleGreen);
-            } catch (BadLocationException e) {
-                e.printStackTrace();
-            }
-
-
-            try {
-                bibliotecary.writeBook();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-
-            try {
-                doc.insertString(doc.getLength(), "Writer(" + id + ") a terminat de scris cartea " + currentBook + ".\n", null);
-            } catch (BadLocationException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        synchronized (Writers.class) {
-            if (!finishMessagePrinted) {
                 try {
-                    doc.insertString(doc.getLength(), "Sesiunea scriitorilor s-a încheiat.\n", null);
+                    doc.insertString(doc.getLength(), "Writer(" + id + ") a început să scrie cartea " + currentBook + ".\n", styleGreen);
                 } catch (BadLocationException e) {
                     e.printStackTrace();
                 }
-                finishMessagePrinted = true;
+
+                try {
+                    bibliotecary.writeBook();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    doc.insertString(doc.getLength(), "Writer(" + id + ") a terminat de scris cartea " + currentBook + ".\n", null);
+                } catch (BadLocationException e) {
+                    e.printStackTrace();
+                }
             }
-        }
 
+            synchronized (Writers.class) {
+                if (!finishMessagePrinted) {
+                    try {
+                        doc.insertString(doc.getLength(), "Sesiunea scriitorilor s-a încheiat.\n", null);
+                    } catch (BadLocationException e) {
+                        e.printStackTrace();
+                    }
+                    finishMessagePrinted = true;
+                }
+            }
 
-        try {
-            doc.insertString(doc.getLength(), "Writer(" + id + ") părăsește biblioteca.\n", null);
+            try {
+                doc.insertString(doc.getLength(), "Writer(" + id + ") părăsește biblioteca.\n", null);
+            } catch (BadLocationException e) {
+                e.printStackTrace();
+            }
+        } catch (InterruptedException e) {
+            try {
+                doc.insertString(doc.getLength(), "Writer(" + id + ") a fost întrerupt deoarece biblioteca era ocupată.\n", null);
+            } catch (BadLocationException ex) {
+                ex.printStackTrace();
+            }
         } catch (BadLocationException e) {
             e.printStackTrace();
+        } finally {
+            if (acquiredLock) {
+                bibliotecary.exitLibrary();
+            }
         }
     }
 }
