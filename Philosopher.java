182d24e43d7eddee6f41717e2015a2f8a2b8951a
Oleg <ol.turevici@gmail.com>
2025-05-12 10:14:34 +0300

    fisierul filosofer

---
+++ Philosopher.java
@@ -0,0 +1,60 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+class Fork {
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void pickUp() {
+        lock.lock();
+    }
+
+    public void putDown() {
+        lock.unlock();
+    }
+}
+
+public class Philosopher implements Runnable {
+    private final int id;
+    private final Fork leftFork;
+    private final Fork rightFork;
+    private final int cycles;
+
+    public Philosopher(int id, Fork leftFork, Fork rightFork, int cycles) {
+        this.id = id;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+        this.cycles = cycles;
+    }
+
+    private void think() throws InterruptedException {
+        System.out.println("Philosopher " + id + " is thinking.");
+        Thread.sleep((int)(Math.random() * 100));
+    }
+
+    private void eat() throws InterruptedException {
+        System.out.println("Philosopher " + id + " is eating.");
+        Thread.sleep((int)(Math.random() * 100));
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < cycles; i++) {
+                think();
+
+                // Evitare deadlock: filosofii iau furculițele într-o ordine controlată
+                Fork first = id % 2 == 0 ? leftFork : rightFork;
+                Fork second = id % 2 == 0 ? rightFork : leftFork;
+
+                first.pickUp();
+                second.pickUp();
+
+                eat();
+
+                second.putDown();
+                first.putDown();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
